c9b0c8e1bb8c2e252bc7691da4ed96f49eadf497
soumyapriyadarshi99 <das.soumyapd@gmail.com>
2023-11-21 23:00:26 +0530

    working except delete

---
+++ task-manager-api/utils/data/useData.js
@@ -0,0 +1,28 @@
+let userData = [
+  //   {
+  //     id: 1,
+  //     title: "nodejs title",
+  //     description: "nodejs description",
+  //     completionStatus: true,
+  //     priorityLevel: "high",
+  //     creationDate: new Date(),
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "javascript title",
+  //     description: "javascript description",
+  //     completionStatus: false,
+  //     priorityLevel: "low",
+  //     creationDate: new Date(),
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "dsAlgo title",
+  //     description: "dsAlgo description",
+  //     completionStatus: false,
+  //     priorityLevel: "medium",
+  //     creationDate: new Date(),
+  //   },
+];
+
+module.exports = userData;
