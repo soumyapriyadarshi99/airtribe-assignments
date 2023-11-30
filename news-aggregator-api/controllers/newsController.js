4fbfbe48f0137e39104c39699a9db5b956ebefb8
soumyapriyadarshi99 <das.soumyapd@gmail.com>
2023-11-30 21:47:26 +0530

    completed everything except news api called

--- news-aggregator-api/controllers/newsController.js
+++ news-aggregator-api/controllers/newsController.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const newsController = express.Router();
+const jwtAuth = require("../middlewares/jwtAuth");
+const userData = require("../utils/data/userData");
 
-newsController.get("/", (req, res) => {
+newsController.get("/", jwtAuth, (req, res) => {
   try {
-    res.status(200).send("sent news");
+    if (req?.user) {
+      const userId = req?.user;
+      const userIndex = userData.findIndex((user) => user?.id === userId);
+      if (userIndex === -1) {
+        return res
+          .status(404)
+          .json({ status: "error", message: "No user found" });
+      }
+      const userPrefernce = userData?.[userIndex]?.preferences;
+      res.status(200).json(userPrefernce);
+    } else {
+      throw new Error(req?.message);
+    }
   } catch (error) {
-    res.status(400).send(error);
+    res.status(403).send(error?.message);
   }
 });
 
